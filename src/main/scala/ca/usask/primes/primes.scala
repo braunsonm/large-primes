574b2bdf3fbff05498724e79080eeeb482bf0789
Braunson Mazoka <bmazoka@gmail.com>
2020-01-27 20:53:33 -0600

    Quick and dirty test code

--- src/main/scala/ca/usask/primes/primes.scala
+++ src/main/scala/ca/usask/primes/primes.scala
@@ -8,6 +8,7 @@ import java.util.logging.Logger
 import org.apache.hadoop
 import org.apache.hadoop.fs.Path
 
+import scala.collection.mutable
 import scala.sys.process._
 
 object primes {
@@ -27,6 +28,9 @@ object primes {
     sc.addFile(program)
 
     val p = new File(dir.getPath + Path.SEPARATOR + "prime.txt")
+    val p2 = new File(dir.getPath + Path.SEPARATOR + "prime2.txt")
+    val p3 = new File(dir.getPath + Path.SEPARATOR + "prime3.txt")
+    var possiblePrimes = mutable.ListBuffer[String]()
     var rand = BigInt(numBits, scala.util.Random)
     val startTime = System.nanoTime()
 
@@ -35,27 +39,60 @@ object primes {
       rand = BigInt(numBits, scala.util.Random)
       rand = rand.setBit(0) // 2^10000 - 2^10001
 
-      val out = new FileWriter(p, false)
+      var out = new FileWriter(p, false)
       out.write(rand.toString)
       out.close()
 
-      val nums = sc.parallelize(List.tabulate(Spark.defaultParallelism)(_ => s"${dir.getPath}${Path.SEPARATOR}prime.txt"))
+      rand = BigInt(numBits, scala.util.Random)
+      rand = rand.setBit(0) // 2^10000 - 2^10001
+
+      out = new FileWriter(p2, false)
+      out.write(rand.toString)
+      out.close()
+
+      rand = BigInt(numBits, scala.util.Random)
+      rand = rand.setBit(0) // 2^10000 - 2^10001
+
+      out = new FileWriter(p3, false)
+      out.write(rand.toString)
+      out.close()
+
+      val nums = sc.parallelize(List.tabulate(Spark.defaultParallelism)(index => s"${dir.getPath}${Path.SEPARATOR}prime${index % 3}.txt"))
       val result = nums.pipe(Seq(s".${Path.SEPARATOR}calc")).collect()
-      var isPrime = true
+      val isPrime = mutable.HashMap[String, Boolean]()
       var i = 0
-      while (isPrime || i < result.length) {
-        if (result(i).contains("0")) {
-          isPrime = false
+      while (isPrime.values.exists(bool => bool) && i < result.length) {
+        if (result(i).split(' ').last.contains("0")) {
+          isPrime(result(i).split(' ').head) = false
         }
         i += 1
       }
 
-      if (isPrime) continue = false
+      if (isPrime.values.exists(bool => bool)) {
+        // Run 50 checks on any that passed
+        possiblePrimes = mutable.ListBuffer[String]()
+        isPrime.filter(tuple => tuple._2).foreach(tuple => {
+          val nums = sc.parallelize(List.tabulate(Spark.defaultParallelism)(tuple._1))
+          val result = nums.pipe(Seq(s".${Path.SEPARATOR}calc")).collect()
+          var passed = true
+          var ii = 0
+          while (passed && ii < result.length) {
+            if (result(ii).split(' ').last.contains("0")) {
+              passed = false
+            }
+            ii += 1
+          }
+
+          if (passed) possiblePrimes.append(tuple._1)
+        })
+
+        if (possiblePrimes.nonEmpty) continue = false
+      }
     }
 
     val endTime = System.nanoTime()
 
     logger.info(s"Prime prob complete. Time elapsed = ${(endTime - startTime) / 1e9}s")
-    logger.info(s"${rand}")
+    possiblePrimes.foreach(logger.info)
   }
 }
